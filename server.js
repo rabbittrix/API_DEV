b3786447ad746fc8f7bae09c0a73c2043ef78a37
Roberto de Souza <rabbittrix@hotmail.com>
2020-09-28 22:10:06 +0100

    update

--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require('express');
 const dotenv = require('dotenv');
 //const logger = require('./middleware/logger')
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db')
 
-// Route files
-const bootcamps = require('./routes/bootcamps')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
+
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 //app.use(logger);
 // Dev logging middleware
@@ -22,8 +31,15 @@ app.use('api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT,
   console.log(
-    `Server listening in ${process.env.NODE_ENV} mode on port ${PORT}`
-  );
+    `Server listening in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow)  
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejections', (err, promise) =>{
+  console.log(`Error: ${err.message}`.red);
+
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });
