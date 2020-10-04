8094dbf39201fca9814f88aae879efcb37fc1adc
Roberto de Souza <rabbittrix@hotmail.com>
2020-10-04 21:53:00 +0100

    Update routes

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,24 +1,44 @@
-const express = require('express');
+const router = require('express').Router();
 const {
-    getBootcamps, 
-    getBootcamp, 
-    createBootcamp, 
-    updateBootcamp, 
-    deleteBootcamp
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius,
+  bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
 
-const router = express.Router();
+// Include other resource routers
+const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
+
+//const router = express.Router();
+
+const advancedResults = require('../middleware/advancedResults');
+const { protect, authorize } = require('../middleware/auth');
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router
-    .route('/')
-    .get(getBootcamps)
-    .post(createBootcamp);
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 router
-    .route('/:id')
-    .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp);
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
+
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
