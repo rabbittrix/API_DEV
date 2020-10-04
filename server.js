ab27da8f3d66b77e8bce368bce49d53cac242254
Roberto de Souza <rabbittrix@hotmail.com>
2020-10-04 21:48:50 +0100

    Update routers

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
-//const logger = require('./middleware/logger')
 const morgan = require('morgan');
 const colors = require('colors');
-const connectDB = require('./config/db')
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
+//const logger = require('./middleware/logger')
 
 
 // Load env vars
@@ -13,12 +23,22 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 // Route files
-const bootcamps = require('./routes/bootcamps')
+const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+const users = require('./routes/users');
+const reviews = require('./routes/reviews');
+const router = require('./routes/courses');
 
 const app = express();
 
+app.use(router);
+
 // Body parser
 app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
 
 //app.use(logger);
 // Dev logging middleware
@@ -26,8 +46,42 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+// File uploading
+app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
